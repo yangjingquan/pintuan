e7f431dd1151d99f203ea2bb9174019fb54217ac
yangjq <yangjq_1991@163.com>
2018-06-27 00:03:53 +0800

    修改了不可描述的文件内容
    Signed-off-by: yangjq <yangjq_1991@163.com>

--- app.js
+++ app.js
@@ -156,20 +156,16 @@ App({
   globalData: {
     userInfo: null,
     bis_id : '31',
-    appid: "wxef01e62c2b3428d5",
-    secret: "d093df2145cdafcec281cf128dca3880",
+    appid: "********",
+    secret: "" ********",",
     openid : '',
     acode : '',
     rec_id : '',
     //测试
-    imgUrl: "http://mall.dxshuju.com:8000/",
-    requestUrl: "https://wxapp.dxshuju.com/index",
-    acodeUrl: "https://wxapp.dxshuju.com/",
-    payUrl: "https://wxapp.dxshuju.com/index/grouppay/pay",
-    //腾讯云正式
-    // imgUrl: "http://cp.dxshuju.com/",
-    // requestUrl: "https://xcx001.dxshuju.com/index",
-    // acodeUrl: "https://xcx001.dxshuju.com/",
-    // payUrl: "https://xcx001.dxshuju.com/index/grouppay/pay",
+    imgUrl: "" ********",",
+    requestUrl: "" ********",",
+    acodeUrl: "" ********",",
+    payUrl: "" ********",",
+    
   }
 })
